abcc9871e4e64d85b6e67f20edf6c896686cf5b6
Ayelen Garcia <ayelengarcia7@gmail.com>
2023-10-11 14:53:05 -0300

    changes

--- src/DAO/models/lista.model.js
+++ src/DAO/models/lista.model.js
@@ -1,6 +1,17 @@
 const mongoose = require("mongoose")
+// const { format } = require("date-fns");
 
 const respuestaSchema = new mongoose.Schema({
+  fecha: {
+    type: String,
+    default: () => {
+      const now = new Date();
+      const day = now.getDate().toString().padStart(2, "0");
+      const month = (now.getMonth() + 1).toString().padStart(2, "0");
+      const year = now.getFullYear().toString();
+      return `${day}/${month}/${year}`;
+    },
+  },
   nombre: String,
   apellido: String,
   celular: String,
@@ -19,9 +30,16 @@ const productoSchema = new mongoose.Schema({
   EAN: String,
 });
 
+const contactoSchema = new mongoose.Schema({
+  frecuencia: String,
+})
+
 const listaSchema = new mongoose.Schema({
   respuesta: respuestaSchema,
-  producto: [productoSchema],
+  recomendados: [productoSchema],
+  suscripcion: String,
+  producto: productoSchema,
+  contacto: contactoSchema,
 });
 
 const ListaModel = mongoose.model("lista", listaSchema);
