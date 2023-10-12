3344354bd163482637d3c9fffc2c2a0833b372af
Ayelen Garcia <ayelengarcia7@hotmail.com>
2023-10-12 20:45:16 -0300

    change model

--- src/DAO/models/lista.model.js
+++ src/DAO/models/lista.model.js
@@ -1,5 +1,4 @@
-const mongoose = require("mongoose")
-// const { format } = require("date-fns");
+const mongoose = require("mongoose");
 
 const respuestaSchema = new mongoose.Schema({
   fecha: {
@@ -9,13 +8,15 @@ const respuestaSchema = new mongoose.Schema({
       const day = now.getDate().toString().padStart(2, "0");
       const month = (now.getMonth() + 1).toString().padStart(2, "0");
       const year = now.getFullYear().toString();
-      return `${day}/${month}/${year}`;
+      const hours = now.getHours().toString().padStart(2, "0");
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      return `${day}/${month}/${year} ${hours}:${minutes}`;
     },
   },
   nombre: String,
   apellido: String,
   celular: String,
-  email:{
+  email: {
     type: String,
     unique: false,
   },
@@ -28,15 +29,21 @@ const respuestaSchema = new mongoose.Schema({
 const productoSchema = new mongoose.Schema({
   titulo: String,
   EAN: String,
+  descuento: Number,
 });
 
 const contactoSchema = new mongoose.Schema({
+  dni: Number,
+  calle: String,
+  altura: Number,
+  piso: String,
+  localidad: String,
+  CP: String,
   frecuencia: String,
-})
+});
 
 const listaSchema = new mongoose.Schema({
   respuesta: respuestaSchema,
-  recomendados: [productoSchema],
   suscripcion: String,
   producto: productoSchema,
   contacto: contactoSchema,
